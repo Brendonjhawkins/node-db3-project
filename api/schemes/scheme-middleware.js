8cd94b88e32bbc7e9c9eb13ac32fc486c14c0f2f
BrendonHawkins <artbitcodedata@gmail.com>
2021-10-07 23:14:05 -0400

    fixed a 404 issue

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -17,7 +17,7 @@ const existing = await db('schemes')
 if(!existing){
   next({
     status: 404,
-    message: `scheme with scheme_id ${req.params.scheme_id}`
+    message: `scheme with scheme_id ${req.params.scheme_id} not found`
   })
 } else {
   next()
